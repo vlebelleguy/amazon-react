00749729589665107ec18915d0a9b25f3c072ff4
Valentin Le Belleguy <lebelleguy.v@gmail.com>
2021-07-25 22:17:40 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    // BEM
+    <div className="App">
+      <Header />
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
