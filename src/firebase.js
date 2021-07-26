3c29a972aaef14dcbc7a0e2aa77b5b14cfe1f466
Valentin Le Belleguy <lebelleguy.v@gmail.com>
2021-07-26 12:33:57 +0200

    update

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBam59Fg9kIiLCrW6yisaFxwmXLsnbsBrw",
-  authDomain: "clone-ad1e9.firebaseapp.com",
-  projectId: "clone-ad1e9",
-  storageBucket: "clone-ad1e9.appspot.com",
-  messagingSenderId: "627098774953",
-  appId: "1:627098774953:web:1c7aa4e5488dac0be6a8b8",
-};
